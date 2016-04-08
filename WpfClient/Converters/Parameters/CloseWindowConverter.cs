fbad75ff2734a9595ca13977e8960db5baeaf9d9
Loïc Montagne <loic.montyagne@oyosoft.fr>
2016-04-08 08:47:07 +0200

    .

---
+++ WpfClient/Converters/Parameters/CloseWindowConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Oyosoft.AgenceImmobiliere.WpfClient.Converters.Parameters
+{
+    public class CloseWindowConverter : IMultiValueConverter
+    {
+        // Paramètres à passer :
+        //    0. Instance de la fenêtre courante (obl)
+        //    1. Résultat de la fenêtre courante (obl)
+        //    2. Nom de la propriété de la fenêtre parente à affecter si le résultat == true et qu'une fenêtre parente existe
+        //    3. Instance à affecter à la propriété nommée de la fenêtre parente si un nom de propriété est fourni, si le résultat == true et si une fenêtre parente existe
+
+        private Window _currentWindow;
+        private bool? _currentWindowResult;
+        private string _affectPropertyName;
+        private object _affectValue;
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values.Length < 2) return values;
+
+            _currentWindow = (Window)values[0];
+            _currentWindowResult = (bool?)values[1];
+
+            _affectPropertyName = null;
+            _affectValue = null;
+            if (values.Length>=4)
+            {
+                _affectPropertyName = (string)values[2]; ;
+                _affectValue = values[3];
+            }
+
+            return new Core.Commands.Command(CloseWindow);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        private async Task CloseWindow()
+        {
+            Window parent = _currentWindow.Owner;
+
+            _currentWindow.DialogResult = _currentWindowResult;
+            _currentWindow.Close();
+
+            if (parent != null && !string.IsNullOrEmpty(_affectPropertyName) && _currentWindowResult != null && (bool)_currentWindowResult)
+            {
+                PropertyInfo prop = parent.GetType().GetProperty(_affectPropertyName);
+                if (prop != null)
+                {
+                    prop.SetValue(parent, _affectValue);
+                }
+            }
+
+        }
+    }
+}
---
+++ WpfClient/Converters/Parameters/OpenWindowConverter.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Oyosoft.AgenceImmobiliere.WpfClient.Converters.Parameters
+{
+    public class OpenWindowConverter : IMultiValueConverter
+    {
+
+        // Paramètres à passer :
+        //    0. Instance de la fenêtre courante (obl)
+        //    1. Type de la fenêtre à ouvrir (obl)
+        //    2. Mode d'ouverture de la fenêtre (par défaut : modal)
+        //    3. Instance ou type du viewmodel associé à la fenêtre à ouvrir
+        //    4 - ~. Paramètres à passer pour le constructeur du viewmodel
+
+
+        public enum OpenMode
+        {
+            Modal = 0,
+            NotModal = 1,
+            CloseParent = 2
+        }
+
+
+        private Window _currentWindow;
+        private Type _newWindowType;
+        private OpenMode _mode;
+        private Type _viewModelType;
+        private object _viewModel;
+        private List<object> _ctorParameters;
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values.Length < 3) return values;
+
+            _currentWindow = (Window)values[0];
+            _newWindowType = (Type)values[1];
+            if (!Enum.TryParse(values[2].ToString(), out _mode)) _mode = OpenMode.Modal;
+
+
+            _viewModelType = null;
+            _viewModel = null;
+            if (values.Length >= 4)
+            {
+                if (values[3] != null)
+                {
+                    if (values[3].GetType() == typeof(Type))
+                    {
+                        _viewModelType = (Type)values[3];
+                    }
+                    else
+                    {
+                        _viewModel = values[3];
+                    }
+                }
+                
+            }
+
+            if (values.Length > 4)
+            {
+                _ctorParameters = new List<object>();
+                for (int i = 4; i < values.Length; i++)
+                {
+                    _ctorParameters.Add(values[i]);
+                }
+            }
+
+            return new Core.Commands.Command(OpenWindow);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        private async Task OpenWindow()
+        {
+            ConstructorInfo ctor = _newWindowType.GetConstructor(new Type[] { });
+            Window newWindow = (Window)ctor.Invoke(new object[] { });
+
+            
+            if (_viewModel == null && _viewModelType != null)
+            {
+                if (_ctorParameters.Count == 0)
+                {
+                    ctor = _viewModelType.GetConstructor(new Type[] { });
+                    _viewModel = ctor.Invoke(new object[] { });
+                }
+                else
+                {
+                    List<Type> types = new List<Type>();
+                    foreach (object param in _ctorParameters)
+                    {
+                        if (param == null)
+                        {
+                            types.Add(typeof(object));
+                        }
+                        else
+                        {
+                            types.Add(param.GetType());
+                        }
+                    }
+                    ctor = _viewModelType.GetConstructor(types.ToArray());
+                    _viewModel = ctor.Invoke(_ctorParameters.ToArray());
+                }
+            }
+            if (_viewModel != null) newWindow.DataContext = _viewModel;
+
+            switch (_mode)
+            {
+                case OpenMode.Modal:
+                    newWindow.Owner = _currentWindow;
+                    newWindow.ShowDialog();
+                    return;
+                case OpenMode.NotModal:
+                    newWindow.Owner = _currentWindow;
+                    newWindow.Show();
+                    return;
+                case OpenMode.CloseParent:
+                    newWindow.Show();
+                    _currentWindow.Close();
+                    return;
+            }
+
+            newWindow = null;
+        }
+    }
+}
